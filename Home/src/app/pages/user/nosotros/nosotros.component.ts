540169eac97284aa38626ca454bd153f7c235450
ADZF28 <adzf2804@gmail.com>
2020-10-20 10:52:41 -0500

    Nosotros

--- Home/src/app/pages/user/nosotros/nosotros.component.ts
+++ Home/src/app/pages/user/nosotros/nosotros.component.ts
@@ -40,31 +40,7 @@ export class NosotrosComponent implements OnInit {
   }
   ];
   
-  ngOnInit(): void {
-    var body = document.getElementsByTagName("body")[0];
-    body.classList.add("index-page");
-
-    var slider = document.getElementById("sliderRegular");
-
-    noUiSlider.create(slider, {
-      start: 40,
-      connect: false,
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
-
-    var slider2 = document.getElementById("sliderDouble");
-
-    noUiSlider.create(slider2, {
-      start: [20, 60],
-      connect: true,
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
+  ngOnInit() {
   }
   ngOnDestroy() {
     var body = document.getElementsByTagName("body")[0];
